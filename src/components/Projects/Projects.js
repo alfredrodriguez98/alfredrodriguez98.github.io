5e7b0c9bd3b8a7596b632da86517d325ff17c72e
alfredrodriguez98 <alfredrodriguez07@outlook.com>
2023-04-20 10:14:46 +0530

    Typo correction

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -22,7 +22,7 @@ function Projects() {
           My Recent <strong className="purple">Works </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on to solve a real life problem.
+          Here are a few projects I've worked on to solve a real life problems.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
